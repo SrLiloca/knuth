075fa1f7ae46fcb95788963fe905c33b20177d7e
SrLiloca <livianpsousal@email.com>
2025-04-03 01:30:36 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+@bp.route('/lista')
+def lista():
+    return render_template('lista.html')
+
+@bp.route('/arvore')
+def arvore():
+    return render_template('arvore.html')
+
+@bp.route('/hashing')
+def hashing():
+    return render_template('hashing.html')
